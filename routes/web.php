124d03dce0288d8f1a21d4b6ddfa5cd9f55dde3b
Denilson-ux <denilsonquichu@gmail.com>
2026-01-01 17:04:14 -0400

    feat: Agregar dashboard con stats cards, controlador y rutas configuradas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
+// Redirect root to login
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+// Authentication Routes (Laravel UI generates these)
+Auth::routes();
+
+// Protected Routes
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
 });
